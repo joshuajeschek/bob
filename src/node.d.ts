78d8a3b3c5a7a04b2d5617558b96a0090b9b2d3f
Joshua Jeschek <joshua@jeschek.eu>
2023-06-13 12:36:34 +0200

    fix: add missing env vars to node.d.ts and add .env.template

--- src/node.d.ts
+++ src/node.d.ts
@@ -2,5 +2,7 @@ declare namespace NodeJS {
 	interface ProcessEnv {
 		NODE_ENV: 'development' | 'production';
 		readonly DISCORD_TOKEN: string;
+		readonly DATABASE_URL: string;
+		readonly PORT?: string;
 	}
 }
